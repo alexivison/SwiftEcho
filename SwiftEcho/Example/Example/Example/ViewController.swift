20bf44ba6478d22d0f017f6fdb20482f04b279d5
tuominen-aleksi <tuominen-aleksi@dmm.com>
2019-05-20 12:23:42 +0900

    Fine tune examples

--- SwiftEcho/Example/Example/Example/ViewController.swift
+++ SwiftEcho/Example/Example/Example/ViewController.swift
@@ -10,22 +10,43 @@ import UIKit
 import SwiftEcho
 
 class ViewController: UIViewController {
+    
+    private var echo: Echo!
+    private let chatId: Int = 123
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Options
         let options: [String: Any] = [
-            "host":"http://localhost:6001",
+            "host": "https://host.address.com", // Replace with Your host
             "auth": [
                 "headers": [
-                    "Authorization": "Bearer " + "Auth token"
+                    "Authorization": "Bearer " + "token" // If the server uses auth, replace with a valid token
                 ]
             ]
         ]
-        let echo = Echo(options: options)
         
-        echo.connected() { (data, ack) in
-            print("CONNECTED")
+        // Init
+        self.echo = Echo(options: options)
+        self.echo.connected { (data, _) in
+            // Default channel & Private channel
+            self.echo
+                .listen(channel: "chat.\(self.chatId)", event: "comment.created") { (comment, _) in
+                    print("RESPONSE: ", comment)
+                }.unsubscribe()
+            // Presence channel
+            self.echo
+                .join(channel: "chat.\(self.chatId)")
+                .here(callback: { (data, ack) in
+                    print("HERE")
+                })
+                .joining(callback: { (data, ack) in
+                    print("Joining")
+                })
+                .leaving(callback: { (data, ack) in
+                    print("Leaving")
+                })
         }
     }
 }
